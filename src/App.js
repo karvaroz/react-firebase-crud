64f3017eab43a112fdc42a4bfb5a5cd0cfb0d80b
karvaroz <vargasriosk@gmail.com>
2022-09-25 21:29:28 -0500

    form login

--- src/App.js
+++ src/App.js
@@ -10,25 +10,25 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import PublicRoute from "./routes/PublicRoute";
 
 const App = () => {
-	const [checking, setChecking] = useState(true);
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	//  const [checking, setChecking] = useState(true);
+	//  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	useEffect(() => {
-		const auth = getAuth();
-		onAuthStateChanged(auth, (user) => {
-			if (user?.uid) {
-				console.log("User is logged in");
-				setIsLoggedIn(true);
-			} else {
-				setIsLoggedIn(false);
-			}
-			setChecking(false);
-		});
-	}, [setIsLoggedIn, setChecking, isLoggedIn]);
+	//  useEffect(() => {
+	//  	const auth = getAuth();
+	//  	onAuthStateChanged(auth, (user) => {
+	//  		if (user?.uid) {
+	//  			console.log("User is logged in");
+	//  			setIsLoggedIn(true);
+	//  		} else {
+	//  			setIsLoggedIn(false);
+	//  		}
+	//  		setChecking(false);
+	//  	});
+	//  }, [setIsLoggedIn, setChecking, isLoggedIn]);
 
-	if (checking) {
-		return <h1>Espere....</h1>;
-	}
+	//  if (checking) {
+	//  	return <h1>Espere....</h1>;
+	//  }
 	return (
 		<BrowserRouter>
 			<Routes>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,21 +2,50 @@ import Lottie from "react-lottie";
 import animationData from "../assets/home.json";
 
 const Login = () => {
-  	const defaultOptions = {
-			loop: true,
-			autoplay: true,
-			animationData: animationData,
-			rendererSettings: {
-				preserveAspectRatio: "xMidYMid slice",
-			},
-		};
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData,
+		rendererSettings: {
+			preserveAspectRatio: "xMidYMid slice",
+		},
+	};
 	return (
-		<div>
-			<Lottie
-				options={defaultOptions}
-				height={400}
-				width={400}
-			/>
+		<div className="row container p-4">
+			<div className="col-md-8">
+				<Lottie
+					options={defaultOptions}
+					height={400}
+					width={400}
+				/>
+			</div>
+			<div className="col-md-4">
+				<div className="mt-5 ms-5">
+					<h3>Iniciar Sesión</h3>
+					<form action="">
+						<div className="mb-3">
+							<label htmlFor="email">Correo Electrónico</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="test@test.com"
+								id="email"
+								name="email"
+							/>
+						</div>
+						<div className="mb-3">
+							<label htmlFor="password">Contraseña</label>
+							<input
+								type="password"
+								className="form-control"
+								id="password"
+								name="password"
+								placeholder="**********"
+							/>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
